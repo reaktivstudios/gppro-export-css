58cea162c0d14d935f3bab388ebdf01e3f54a4ea
Nick Croft <nick@copyblogger.com>
2018-04-09 11:24:03 -0400

    manual phpcs fixes.

--- gppro-export-css.php
+++ gppro-export-css.php
@@ -1,29 +1,32 @@
 <?php
-/*
-Plugin Name: Genesis Design Palette Pro - Export CSS
-Plugin URI: https://genesisdesignpro.com/
-Description: Adds a button to export raw CSS file
-Author: Reaktiv Studios
-Version: 1.0.2
-Requires at least: 3.7
-Author URI: http://andrewnorcross.com
-*/
-/*
-  Copyright 2014 Andrew Norcross
+/**
+ * Plugin Name: Genesis Design Palette Pro - Export CSS
+ * Plugin URI: https://genesisdesignpro.com/
+ * Description: Adds a button to export raw CSS file
+ * Author: Reaktiv Studios
+ * Version: 1.0.2
+ * Requires at least: 3.7
+ * Author URI: http://andrewnorcross.com
+ *
+ * @package gppro-export-css
+ */
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; version 2 of the License (GPL v2) only.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/**
+ * Copyright 2014 Andrew Norcross
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License (GPL v2) only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 if ( ! defined( 'GPXCS_BASE' ) ) {
 	define( 'GPXCS_BASE', plugin_basename( __FILE__ ) );
@@ -37,42 +40,40 @@ if ( ! defined( 'GPXCS_VER' ) ) {
 	define( 'GPXCS_VER', '1.0.2' );
 }
 
-
+/**
+ * Class GP_Pro_Export_CSS.
+ */
 class GP_Pro_Export_CSS {
 
-
 	/**
-	 * Static property to hold our singleton instance
+	 * Static property to hold our singleton instance.
 	 *
 	 * @var GP_Pro_Export_CSS
 	 */
-	static $instance = false;
+	public static $instance = false;
 
 	/**
-	 * This is our constructor
-	 *
-	 * @return GP_Pro_Export_CSS
+	 * This is our constructor.
 	 */
 	private function __construct() {
 
-		// general backend
+		// General backend.
 		add_action( 'plugins_loaded', array( $this, 'textdomain' ) );
 		add_action( 'admin_init', array( $this, 'export_css_file' ) );
 		add_action( 'admin_notices', array( $this, 'gppro_active_check' ), 10 );
 		add_action( 'admin_notices', array( $this, 'export_css_notices' ) );
 		add_action( 'admin_head', array( $this, 'export_css_style' ) );
 
-		// GP Pro specific
+		// GP Pro specific.
 		add_filter( 'dpp_settings', array( $this, 'export_css_section' ), 15, 2 );
 	}
 
 	/**
-	 * If an instance exists, this returns it.  If not, it creates one and
-	 * retuns it.
+	 * If an instance exists, this returns it.  If not, it creates one and returns it.
 	 *
 	 * @return GP_Pro_Export_CSS
 	 */
-	public static function getInstance() {
+	public static function get_instance() {
 		if ( ! self::$instance ) {
 			self::$instance = new self();
 		}
@@ -80,52 +81,44 @@ class GP_Pro_Export_CSS {
 	}
 
 	/**
-	 * load textdomain
-	 *
-	 * @return
+	 * Load textdomain.
 	 */
 	public function textdomain() {
 		load_plugin_textdomain( 'gppro-export-css', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
-	 * check for GP Pro being active
-	 *
-	 * @return GP_Pro_Export_CSS
+	 * Check for GP Pro being active.
 	 */
 	public function gppro_active_check() {
-		// get the current screen
+		// Get the current screen.
 		$screen = get_current_screen();
-		// bail if not on the plugins page
-		if ( is_object( $screen ) && $screen->parent_file !== 'plugins.php' ) {
+		// Bail if not on the plugins page.
+		if ( is_object( $screen ) && 'plugins.php' !== $screen->parent_file ) {
 			return;
 		}
-		// run the active check
+		// Run the active check.
 		$coreactive = class_exists( 'Genesis_Palette_Pro' ) ? Genesis_Palette_Pro::check_active() : false;
-		// active. bail
+		// Active. Bail.
 		if ( $coreactive ) {
 			return;
 		}
-		// not active. show message
-		echo '<div id="message" class="error fade below-h2"><p><strong>' . __( sprintf( 'This plugin requires Genesis Design Palette Pro to function and cannot be activated.' ), 'gppro-export-css' ) . '</strong></p></div>';
-		// hide activation method
+		// Not active. Show message.
+		echo '<div id="message" class="error fade below-h2"><p><strong>' . esc_html__( 'This plugin requires Genesis Design Palette Pro to function and cannot be activated.', 'gppro-export-css' ) . '</strong></p></div>';
+		// Hide activation method.
 		unset( $_GET['activate'] );
-		// deactivate the plugin
+		// Deactivate the plugin.
 		deactivate_plugins( plugin_basename( __FILE__ ) );
-		// and finish
-		return;
 	}
 
 	/**
-	 * add CSS to the admin head
-	 *
-	 * @return CSS
+	 * Add CSS to the admin head.
 	 */
 	public function export_css_style() {
-		// fetch current screen
+		// Fetch current screen.
 		$screen = get_current_screen();
-		// display our admin UI CSS if on DPP page
-		if ( is_object( $screen ) && $screen->base == 'genesis_page_genesis-palette-pro' ) {
+		// Display our admin UI CSS if on DPP page.
+		if ( is_object( $screen ) && 'genesis_page_genesis-palette-pro' === $screen->base ) {
 			echo '<style media="all" type="text/css">';
 			echo 'a.gppro-css-export-view{display:inline-block;margin-left:5px;text-decoration:none;}';
 			echo '</style>';
@@ -133,81 +126,77 @@ class GP_Pro_Export_CSS {
 	}
 
 	/**
-	 * display messages if export failure
+	 * Display messages if export failure.
 	 *
 	 * @return mixed
 	 */
 	public function export_css_notices() {
 
-		// first check to make sure we're on our settings
-		if ( ! isset( $_REQUEST['page'] ) || isset( $_REQUEST['page'] ) && $_REQUEST['page'] !== 'genesis-palette-pro' ) {
+		// First check to make sure we're on our settings.
+		if ( ! isset( $_REQUEST['page'] ) || isset( $_REQUEST['page'] ) && 'genesis-palette-pro' !== $_REQUEST['page'] ) { // WPCS: csrf ok.
 			return;
 		}
 
-		// check our CSS export action
-		if ( ! isset( $_REQUEST['export-css'] ) ) {
+		// Check our CSS export action.
+		if ( ! isset( $_REQUEST['export-css'] ) ) { // WPCS: csrf ok.
 			return;
 		}
 
-		// check for non failure
-		if ( isset( $_REQUEST['export-css'] ) && $_REQUEST['export-css'] != 'failure' ) {
+		// Check for non failure.
+		if ( isset( $_REQUEST['export-css'] ) && 'failure' !== $_REQUEST['export-css'] ) { // WPCS: csrf ok.
 			return;
 		}
 
-		// check for failure
-		if ( isset( $_REQUEST['export-css'] ) && $_REQUEST['export-css'] == 'failure' ) {
+		// Check for failure.
+		if ( isset( $_REQUEST['export-css'] ) && 'failure' === $_REQUEST['export-css'] ) { // WPCS: csrf ok.
 
-			// set a default message
+			// Set a default message.
 			$message = __( 'There was an error with your export. Please try again later.', 'gppro-export-css' );
 
-			// no parent class present
-			if ( $_REQUEST['reason'] == 'noclass' ) {
+			// No parent class present.
+			if ( 'noclass' === $_REQUEST['reason'] ) { // WPCS: csrf ok.
 				$message = __( 'The main Genesis Design Palette Pro files are not present.', 'gppro-export-css' );
 			}
 
-			// no data stored
-			if ( $_REQUEST['reason'] == 'nodata' ) {
+			// No data stored.
+			if ( 'nodata' === $_REQUEST['reason'] ) { // WPCS: csrf ok.
 				$message = __( 'No settings data has been saved. Please save your settings and try again.', 'gppro-export-css' );
 			}
 
-			// no CSS file present
-			if ( $_REQUEST['reason'] == 'nofile' ) {
+			// No CSS file present.
+			if ( 'nofile' === $_REQUEST['reason'] ) { // WPCS: csrf ok.
 				$message = __( 'No CSS file exists to export. Please save your settings and try again.', 'gppro-export-css' );
 			}
 
-			// return the message
+			// Return the message.
 			echo '<div id="message" class="error">';
 			echo '<p>' . esc_attr( $message ) . '</p>';
 			echo '</div>';
 
 			return;
 		}
-
-		return;
 	}
 
 	/**
-	 * export our CSS file
+	 * Export our CSS file.
 	 *
 	 * @return mixed
 	 */
 	public function export_css_file() {
-
-		// check page and query string
-		if ( ! isset( $_REQUEST['gppro-css-export'] ) || isset( $_REQUEST['gppro-css-export'] ) && $_REQUEST['gppro-css-export'] != 'go' ) {
+		// Check nonce.
+		if ( empty( $_REQUEST['_wpnonce'] ) || ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'gppro_css_export_nonce' ) ) {
 			return;
 		}
 
-		// check nonce
-		$nonce = $_REQUEST['_wpnonce'];
-		if ( ! wp_verify_nonce( $nonce, 'gppro_css_export_nonce' ) ) {
+		// Check page and query string.
+		if ( ! isset( $_REQUEST['gppro-css-export'] ) || isset( $_REQUEST['gppro-css-export'] ) && 'go' !== $_REQUEST['gppro-css-export'] ) {
 			return;
 		}
 
-		// get current settings
+		// Get current settings.
 		$current = get_option( 'gppro-settings' );
 
-		// if settings empty, bail
+		// If settings empty, bail.
 		if ( empty( $current ) ) {
 			$failure = menu_page_url( 'genesis-palette-pro', 0 ) . '&section=build_settings&export-css=failure&reason=nodata';
 			wp_safe_redirect( $failure );
@@ -215,7 +204,7 @@ class GP_Pro_Export_CSS {
 			return;
 		}
 
-		// check for class
+		// Check for class.
 		if ( ! class_exists( 'Genesis_Palette_Pro' ) ) {
 			$failure = menu_page_url( 'genesis-palette-pro', 0 ) . '&section=build_settings&export-css=failure&reason=noclass';
 			wp_safe_redirect( $failure );
@@ -232,20 +221,20 @@ class GP_Pro_Export_CSS {
 			return;
 		}
 
-		// * Prepare and send the export file to the browser
+		// Prepare and send the export file to the browser.
 		header( 'Content-Description: File Transfer' );
 		header( 'Cache-Control: public, must-revalidate' );
 		header( 'Pragma: hack' );
 		header( 'Content-type: text/css; charset=utf-8' );
 		header( 'Content-Disposition: attachment; filename="gppro-custom.css"' );
 		header( 'Content-Length: ' . mb_strlen( $output ) );
-		echo str_replace( '&quot;', '"', esc_html( $output ) );
+		echo str_replace( '&quot;', '"', esc_html( $output ) ); // WPCS: xss ok.
 		exit();
 	}
 
 
 	/**
-	 * add new option for exporting CSS
+	 * Add new option for exporting CSS.
 	 *
 	 * @param  array $settings The DPP settings array.
 	 * @return array
@@ -257,66 +246,41 @@ class GP_Pro_Export_CSS {
 			'callback' => array( $this, 'export_css_input' ),
 		);
 
-		/*
-		 add section header for export
-		$items['section-break-css-export']	= array(
-			'break'	=> array(
-				'type'	=> 'full',
-				'title'	=> __( '', 'gppro-export-css' ),
-				'text'	=> __( 'Download a stand-alone CSS file', 'gppro-export-css' ),
-			),
-		);
-
-		// add button for export
-		$items['css-export-area-setup']	= array(
-			'title'		=> '',
-			'data'		=> array(
-				'css-export-field'	=> array(
-					'label'		=> __( 'Download CSS file', 'gppro-export-css' ),
-					'button'	=> __( 'Export CSS', 'gppro-export-css' ),
-					'input'		=> 'custom',
-					'callback'	=>
-				),
-			),
-		);*/
-
 		return $settings;
 	}
 
 	/**
 	 * Create input field for CSS export.
-	 *
-	 * @return
 	 */
-	static function export_css_input() {
+	public function export_css_input() {
 
-		// first check for the data
+		// First check for the data.
 		$saved = get_option( 'gppro-settings' );
-		// display message without saved options
+		// Display message without saved options.
 		if ( empty( $saved ) ) {
 			$text = __( 'No data has been saved. Please save your settings before attempting to export.', 'gppro-export-css' );
 			return '<div class="gppro-input gppro-description-input"><p class="description">' . esc_attr( $text ) . '</p></div>';
 		}
 
-		// get my values
+		// Get my values.
 		$id     = 'gppro-export-css';
 		$name   = 'gppro-export-css';
 		$button = __( 'Export File', 'gppro-export-css' );
 
-		// get CSS file for link
+		// Get CSS file for link.
 		$file_key = get_theme_mod( 'dpp_file_key' );
 
-		// create export URL with nonce
+		// Create export URL with nonce.
 		$expnonce = wp_create_nonce( 'gppro_css_export_nonce' );
 
-		// set the empty
+		// Set the empty.
 		$input = '';
-		// begin markup
+		// Begin markup.
 		$input .= '<div class="gppro-input gppro-css-export-input gppro-setting-input">';
 
-		// handle browser link
+		// Handle browser link.
 		if ( ! empty( $file_key ) ) {
-			// handle label with optional CSS file link
+			// Handle label with optional CSS file link.
 			$input .= '<div class="gppro-input-item gppro-input-wrap"><p class="description">';
 
 			$input .= esc_html__( 'View CSS file', 'gppro-export-css' );
@@ -328,7 +292,7 @@ class GP_Pro_Export_CSS {
 			$input .= '</p></div>';
 		}
 
-			// display button
+			// Display button.
 			$input     .= '<div class="gppro-input-item gppro-input-label choice-label">';
 				$input .= '<span class="gppro-settings-button">';
 
@@ -337,17 +301,17 @@ class GP_Pro_Export_CSS {
 				$input .= '</span>';
 			$input     .= '</div>';
 
-		// close markup
+		// Close markup.
 		$input .= '</div>';
 
-		// send it back
+		// Send it back.
 		echo $input; // WPCS: xss ok.
 
 	}
 
-	// end class
+	// End class.
 }
 
-// Instantiate our class
-$GP_Pro_Export_CSS = GP_Pro_Export_CSS::getInstance();
+// Instantiate our class.
+$gp_pro_export_css = GP_Pro_Export_CSS::get_instance();
 
